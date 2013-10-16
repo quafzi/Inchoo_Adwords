3c99695da2f6d50e5cb101fee8bc2b9e1fa99ad7
Thomas Birke <tbirke@netextreme.de>
2013-10-16 11:47:52 +0200

    Move config settings and use template instead of in-block HTML generation

---
+++ app/code/community/Inchoo/Adwords/Block/Conversion.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @category   Google
+ * @package    Inchoo_Adwords
+ * @author     Thomas Birke <tbirke@netextreme.de>
+ */
+class Inchoo_Adwords_Block_Conversion extends Mage_Core_Block_Template
+{
+	public function isTrackingAllowed()
+    {
+        return (bool)(int)$this->getConfig('conversion_tracking_enabled');
+    }
+
+    public function getConversionId()
+    {
+        return $this->getConfig('conversion_id');
+    }
+
+    public function getLanguage()
+    {
+        return $this->getConfig('conversion_language');
+    }
+
+    public function getFormat()
+    {
+        return $this->getConfig('conversion_format');
+    }
+
+    public function getColor()
+    {
+        return $this->getConfig('conversion_color');
+    }
+
+    public function getLabel()
+    {
+        return $this->getConfig('conversion_label');
+    }
+
+    public function getRemarketingOnly()
+    {
+        return $this->getConfig('conversion_remarketing_only')
+            ? 'true'
+            : 'false';
+    }
+
+    protected function getConfig($key)
+    {
+        return Mage::getStoreConfig('google/analytics/' . $key);
+    }
+	
+	public function getOrderTotal()
+	{
+        $orderId = (int) Mage::getSingleton('checkout/session')
+            ->getLastOrderId();
+        if (!$orderId) {
+            return 0;
+        }
+        $order = Mage::getModel('sales/order')->load($orderId);
+        if ($order->getId()) {
+            return round($order->getGrandTotal(), 2);
+        }
+        return 0;
+	}
+}
